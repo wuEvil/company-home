9c12f038d4ae33b918b314689167959ea6c6e841
yym1745 <wo985417@qq.com>
2018-05-09 22:13:01 +0800

    2018年5月9日 22:12:57

--- 项目进度/gys/fatuan.js
+++ 项目进度/gys/fatuan.js
@@ -1,41 +1,10 @@
-var str = {};
-// var shuangchuanlujing = "http://192.168.1.107:8080/oyetravel/"; //看拼音
-function su() {
-
-    $.each($(".form-control"), function (i, d) {
-        // console.log(i, d)
-        if ($(d).val() == "") {
-            alert("资料没有填完")
-            return false;
-        } else {
-            alert("quan")
-            str[$(d).attr('name')] = $(d).val();
-        }
-    });
-    console.log(str)
-
-    // $.ajax({
-    //     type: "POST",
-    //     url: shuangchuanlujing,
-    //     dada: str,
-    //     success: function (a, b, c) {
-    //         // console.log(a, b, c)
-    //         if (c.status == 200) {
-    //             alert("上传成功");
-    //         }
-    //     }
-    // })
-}
-$("input").each(function () {
-    var vl = $(this).val();
-    if (vl == "") {}
-})
-// tu
-
+// 页面样式动态设定固定值
 setTimeout(function () {
+    var wid = $("#tourline_sn").width();
+    console.log(wid)
     $(".am-selected button").attr("class",
         "am-selected-btn am-btn am-dropdown-toggle am-btn-default form-control")
-    $(".am-selected button").css("width", "800px")
+    $(".am-selected button").css("width", wid + "px")
     // console.log($("#ambzd").children())
 }, 100)
 
@@ -57,9 +26,50 @@ $("#btntime1").click(function () {
     }
 })
 
+// 获取值生产弹出层表格
+function aaa() { // 时间地点 这里不统一获取
+    var scr = []
+    var str = {
+        tourline_sn: $("#tourline_sn").val(), //线路编号
+        title: $("#title").val(), //线路标题
+        tourlineHead: $("#tourlineHead").val(), //
+        tourlinePhone: $("#tourlinePhone").val(), //
+        adult_limit: $("#adult_limit").val(), //总人数
+        adult_buy_max: $("#adult_buy_max").val(), //单笔订单最大人数
+        tour_feature: $("#tour_feature").val(), //产品特色
+        price_explain: $("#price_explain").val(), //费用说明
+        travel_restrictions: $("#travel_restrictions").val(), //出游人群限制
+        child_norm: $("#child_norm").val(), //儿童标准
+        is_refund: $("#is_refund").val(), //是否支持退
+        refund_desc: $("#refund_desc").val(), //退票说明
+        refund_method: $("#refund_method").val(), //退款方式
+        appoint_desc: $("#appoint_desc").val(), //服务标准
+        tour_desc: $("#tour_desc").val(), //行程安排
+        tour_range: $("#tour_range").val(), //线路区域
+        tour_type: $("#tour_type").val(), //线路类型
+        cover_img1: $("#jyxkimg").attr("src"), //图片的路径值
+        cover_img2: $("#jyximg").attr("src"),
+        cover_img: $("#jyxkzimg").attr("src"),
+        costPrice: $("#costPrice").val(), //成本价
+        dealPrice: $("#dealPrice").val(), //交接价
+        price: $("#price").val(), //门市价
+        childPrice: $("#childPrice").val(),
+        area_cache: scr
+        // 获取地址
+    }
+    console.log(str)
+    console.log(scr)
 
-
-
-function aaa() {
-    $("#selects").html(`<option>请选择县</option>`)
+    var cityName = $(".am-fl").text()
+    var baidanhtml = ``;
+    console.log(cityName)
+    // $("#selects").html(`<option>请选择县</option>`)
+    layer.open({
+        title: "发团详细价格",
+        // skin: 'layui-layer-demo', //样式类名
+        // closeBtn: 0, //不显示关闭按钮
+        // anim: 2,
+        // shadeClose: true, //开启遮罩关闭
+        content: baidanhtml
+    });
 }
